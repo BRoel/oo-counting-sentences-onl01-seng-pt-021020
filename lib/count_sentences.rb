efea9f86d74b484dd9857e1e8a98a19bd9916c6e
Brennan Roel <brennanroel@gmail.com>
2020-03-01 20:04:09 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,8 +2,12 @@ require 'pry'
 
 class String
 
-  def sentence?
-    
+ def sentence?
+    if self.end_with?(".")
+      return true
+    else
+      return false
+    end
   end
 
   def question?
